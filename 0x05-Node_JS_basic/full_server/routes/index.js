a57576ba2c8c8ab15c6a7f5dd768400e1367258e
blvck-root <mtndlovu81@gmail.com>
2024-08-20 21:24:08 +0200

    fix ESLint violations

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -4,10 +4,10 @@ import StudentsController from '../controllers/StudentsController';
 /**
  * Binds the routes to the appropriate handler in an Express app.
  */
-function mapRoutes(app) {
+function mapRoutes (app) {
   app.get('/', AppController.getHomepage);
   app.get('/students', StudentsController.getAllStudents);
   app.get('/students/:major', StudentsController.getAllStudentsByMajor);
-};
+}
 
 module.exports = mapRoutes;
